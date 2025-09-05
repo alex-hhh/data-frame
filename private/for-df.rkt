e064719e75008c1ffb3c951c041a25e52a4c8847
Alex Harsányi <AlexHarsanyi@gmail.com>
2025-09-05 10:30:38 +0800

    add error reporting to for_/data-frame macro (AB#90)
    ... when for loop body produces wrong number of values compared to the number
    of data frame series.

--- private/for-df.rkt
+++ private/for-df.rkt
@@ -3,6 +3,7 @@
 ;;
 ;; This file is part of data-frame -- https://github.com/alex-hhh/data-frame
 ;; Copyright (c) 2021 Hazel Levine <hazel@knightsofthelambdacalcul.us>
+;; Copyright (c) 2025 Alex Harsányi <alexharsanyi@gmail.com>
 ;;
 ;; This program is free software: you can redistribute it and/or modify it
 ;; under the terms of the GNU Lesser General Public License as published by
@@ -26,28 +27,42 @@
 ;; `for_/fold/derived-stx`, with the given series names `(column:id ...)`.
 (define-for-syntax (for_/data-frame stx for_/fold/derived-stx)
   (syntax-parse stx
-    [(_ (column:id ...) #:initial-capacity capacity:expr clauses body ... tail-expr)
+    [(name:id (column:id ...) #:initial-capacity capacity:expr clauses body ... tail-expr)
      #:with orig-stx stx
      #:with for_/fold/derived for_/fold/derived-stx
      #:with ((pre-body ...) (post-body ...)) (split-for-body stx #'(body ... tail-expr))
      ;; each series name is specified in (column:id ...), convert them to
      ;; strings
-     #'(let ([series (list (make-series (symbol->string 'column) #:capacity capacity) ...)])
+     #'(let* ([series (list (make-series (symbol->string 'column) #:capacity capacity) ...)]
+              [series-count (length series)])
          (for_/fold/derived
           orig-stx
           ((_unused (void)))            ; TODO: how to get rid of this?
           clauses pre-body ...
           (call-with-values
            (lambda () post-body ...)
-           (lambda vs (for-each unsafe-series-push-back! series vs))))
+           (lambda vs
+             (unless (= (length vs) series-count)
+               (raise-result-error
+                (syntax->datum #'name)
+                (format "body produced wrong number of values: got ~a, expecting ~a"
+                        (length vs) series-count)
+                vs))
+             ;; Avoid using for-each, as this would check again the length of
+             ;; its inputs, which is a performance penalty
+             (let loop ([series series]
+                        [vs vs])
+               (unless (null? series)
+                 (unsafe-series-push-back! (car series) (car vs))
+                 (loop (cdr series) (cdr vs)))))))
          (make-data-frame #:series series))]
-    [(_ (column:id ...) clauses body ... tail-expr)
+    [(name:id (column:id ...) clauses body ... tail-expr)
      ;; Note: we use a large(ish) default initial capacity, making data frames
      ;; use more memory for toy projects when a small number of rows are kept
      ;; in a DF.  However, this reduces the number of re-allocations when DF's
      ;; with large number of rows are used, which is what the intended
      ;; purposes of the data frame object is.
-     (for_/data-frame #'(fdf (column ...) #:initial-capacity 256 clauses body ... tail-expr) for_/fold/derived-stx)]
+     (for_/data-frame #'(name (column ...) #:initial-capacity 256 clauses body ... tail-expr) for_/fold/derived-stx)]
     ))
 
 ;;;; for/data-frame and for*/data-frame
